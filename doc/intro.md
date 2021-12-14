28e709af0ddd118480dbce439b38c9ff17d0247c
Cris Jr <crisjr@pm.me>
2021-12-14 10:54:45 -0300

    lein new app br.eng.liberdade.strint

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to br.bsb.liberdade.strint
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/br/bsb/liberdade/strint_test.clj
@@ -0,0 +1,7 @@
+(ns br.bsb.liberdade.strint-test
+  (:require [clojure.test :refer :all]
+            [br.bsb.liberdade.strint :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
